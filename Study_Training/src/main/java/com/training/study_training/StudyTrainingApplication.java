83613960db239f6f61f2db33da6c9082c84f8123
DESKTOP-FO0EP77\sdc13 <sdc13@hanmail.net>
2022-03-29 19:49:01 +0900

    project add to git

---
+++ Study_Training/src/main/java/com/training/study_training/StudyTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.training.study_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTrainingApplication.class, args);
+	}
+
+}
---
+++ Study_Training/src/test/java/com/training/study_training/StudyTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.study_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
